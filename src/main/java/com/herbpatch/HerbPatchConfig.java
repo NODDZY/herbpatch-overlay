8e8567cc0d8e80c19707d59c48837c22bbeb4bcf
matssg <40918636+NODDZY@users.noreply.github.com>
2024-02-28 23:25:48 +0100

    Reorder config

--- src/main/java/com/herbpatch/HerbPatchConfig.java
+++ src/main/java/com/herbpatch/HerbPatchConfig.java
@@ -12,6 +12,109 @@ import java.awt.*;
 public interface HerbPatchConfig extends Config {
     String CONFIG_GROUP = "herbpatchoverlay";
 
+    @ConfigItem(
+        keyName = "overlaySelector",
+        name = "Overlay type",
+        description = "Configure overlay type",
+        position = 101
+    )
+    default OverlayType overlaySelector() {
+        return OverlayType.HULL;
+    }
+
+    @ConfigItem(
+        keyName = "renderIfOvergrown",
+        name = "Render overgrown overlay",
+        description = "Render overlay on overgrown herb patches",
+        position = 102
+    )
+    default boolean renderIfOvergrown() {
+        return true;
+    }
+
+    @ConfigItem(
+        keyName = "renderIfEmpty",
+        name = "Render empty overlay",
+        description = "Render overlay on empty herb patches",
+        position = 103
+    )
+    default boolean renderIfEmpty() {
+        return true;
+    }
+
+    @ConfigSection(
+            name = "Herb Overlay Color",
+            description = "Custom overlay colors for herbs",
+            position = 200
+    )
+    String customColorSection = "customColor";
+
+    @ConfigItem(
+            keyName = "overgrownColor",
+            name = "Overgrown",
+            description = "",
+            section = customColorSection,
+            position = 201
+    )
+    default Color overgrownColor() {
+        return Color.RED;
+    }
+
+    @ConfigItem(
+            keyName = "emptyColor",
+            name = "Empty",
+            description = "",
+            section = customColorSection,
+            position = 202
+    )
+    default Color emptyColor() {
+        return Color.RED;
+    }
+
+    @ConfigItem(
+            keyName = "unripeColor",
+            name = "Unripe",
+            description = "",
+            section = customColorSection,
+            position = 203
+    )
+    default Color unripeColor() {
+        return Color.YELLOW;
+    }
+
+    @ConfigItem(
+            keyName = "matureColor",
+            name = "Mature",
+            description = "",
+            section = customColorSection,
+            position = 204
+    )
+    default Color matureColor() {
+        return Color.GREEN;
+    }
+
+    @ConfigItem(
+            keyName = "diseasedColor",
+            name = "Diseased",
+            description = "",
+            section = customColorSection,
+            position = 205
+    )
+    default Color diseasedColor() {
+        return Color.MAGENTA;
+    }
+
+    @ConfigItem(
+            keyName = "deadColor",
+            name = "Dead",
+            description = "",
+            section = customColorSection,
+            position = 206
+    )
+    default Color deadColor() {
+        return Color.RED;
+    }
+
     @AllArgsConstructor
     enum OverlayType {
         HULL("Hull"),
@@ -24,108 +127,5 @@ public interface HerbPatchConfig extends Config {
         public String toString() {
             return value;
         }
-    }
-
-    @ConfigSection(
-            name = "Herb Overlay Color",
-            description = "Custom overlay colors for herbs",
-            position = 3
-    )
-    String customColorSection = "customColor";
-
-    @ConfigItem(
-            keyName = "overgrownColor",
-            name = "Overgrown",
-            description = "",
-            section = customColorSection,
-            position = 0
-    )
-    default Color overgrownColor() {
-        return Color.RED;
-    }
-
-    @ConfigItem(
-            keyName = "emptyColor",
-            name = "Empty",
-            description = "",
-            section = customColorSection,
-            position = 1
-    )
-    default Color emptyColor() {
-        return Color.RED;
-    }
-
-    @ConfigItem(
-            keyName = "unripeColor",
-            name = "Unripe",
-            description = "",
-            section = customColorSection,
-            position = 2
-    )
-    default Color unripeColor() {
-        return Color.YELLOW;
-    }
-
-    @ConfigItem(
-            keyName = "matureColor",
-            name = "Mature",
-            description = "",
-            section = customColorSection,
-            position = 3
-    )
-    default Color matureColor() {
-        return Color.GREEN;
-    }
-
-    @ConfigItem(
-            keyName = "diseasedColor",
-            name = "Diseased",
-            description = "",
-            section = customColorSection,
-            position = 4
-    )
-    default Color diseasedColor() {
-        return Color.MAGENTA;
-    }
-
-    @ConfigItem(
-            keyName = "deadColor",
-            name = "Dead",
-            description = "",
-            section = customColorSection,
-            position = 5
-    )
-    default Color deadColor() {
-        return Color.RED;
-    }
-
-    @ConfigItem(
-            keyName = "overlaySelector",
-            name = "Overlay type",
-            description = "Configure overlay type",
-            position = 0
-    )
-    default OverlayType overlaySelector() {
-        return OverlayType.HULL;
-    }
-
-    @ConfigItem(
-            keyName = "renderIfOvergrown",
-            name = "Render overgrown overlay",
-            description = "Render overlay on overgrown herb patches",
-            position = 1
-    )
-    default boolean renderIfOvergrown() {
-        return true;
-    }
-
-    @ConfigItem(
-            keyName = "renderIfEmpty",
-            name = "Render empty overlay",
-            description = "Render overlay on empty herb patches",
-            position = 2
-    )
-    default boolean renderIfEmpty() {
-        return true;
     }
 }
