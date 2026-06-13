08c4505b2a96a2ce87ead4047a61050e0093a145
matssg <40918636+NODDZY@users.noreply.github.com>
2026-06-13 19:51:29 +0200

    Change from NullObjectID to api.gameval.ObjectID

--- src/main/java/com/herbpatch/constants/HerbPatch.java
+++ src/main/java/com/herbpatch/constants/HerbPatch.java
@@ -3,23 +3,22 @@ package com.herbpatch.constants;
 import java.util.Arrays;
 import java.util.List;
 
-import static net.runelite.api.NullObjectID.*;
+import static net.runelite.api.gameval.ObjectID.*;
 
 /**
  * Class containing ObjectIDs for the various herb patches around Gilenor
  */
 public class HerbPatch {
-    public static final int FALADOR     = NULL_8150;
-    public static final int PHASMATYS   = NULL_8153;
-    public static final int CATHERBY    = NULL_8151;
-    public static final int ARDOUGNE    = NULL_8152;
-    public static final int HOSIDIUS    = NULL_27115;
-    public static final int STRONGHOLD  = NULL_18816;
-    public static final int HARMONY     = NULL_9372;
-    public static final int WEISS       = NULL_33313;
-    public static final int GUILD       = NULL_33979;
-    public static final int VARLAMORE   = NULL_50697;
+    public static final int FALADOR    = FARMING_HERB_PATCH_1;
+    public static final int CATHERBY   = FARMING_HERB_PATCH_2;
+    public static final int ARDOUGNE   = FARMING_HERB_PATCH_3;
+    public static final int PHASMATYS  = FARMING_HERB_PATCH_4;
+    public static final int HARMONY    = FARMING_HERB_PATCH_5;
+    public static final int STRONGHOLD = MYARM_HERBPATCH;
+    public static final int HOSIDIUS   = FARMING_HERB_PATCH_6;
+    public static final int WEISS      = MY2ARM_HERBPATCH_FAKE;
+    public static final int GUILD      = FARMING_HERB_PATCH_7;
+    public static final int VARLAMORE  = FARMING_HERB_PATCH_8;
 
     public static final List<Integer> allHerbObjects = Arrays.asList(FALADOR, PHASMATYS, CATHERBY, ARDOUGNE, HOSIDIUS, STRONGHOLD, HARMONY, WEISS, GUILD, VARLAMORE);
 }
-
